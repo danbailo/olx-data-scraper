df929cd067de911a077872fe2bebe2e0050a3907
Daniel Bailo <danbailoufms@gmail.com>
2020-02-25 22:28:26 -0400

    insercao de dados programada

--- src/core/database.py
+++ src/core/database.py
@@ -1,4 +1,5 @@
 import psycopg2
+import re
 
 class Database:    
     def __init__(self, dbname, user, password):
@@ -9,33 +10,50 @@ class Database:
             host="localhost")
         self.conn.autocommit = True
         self.cur = self.conn.cursor()
-        self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS test ("
-                "id_anuncio INTEGER,"
-                "municipio VARCHAR(64),"
-                "estado CHAR(2),"
-                "cep SMALLINT,"
-                "preco NUMERIC(14,2),"
-                "area VARCHAR(16),"
-                "tipo VARCHAR(32),"
-                "titulo VARCHAR(64),"
-                "descricao TEXT,"
-                "fotos TEXT [],"
-                "ddd CHAR(2),"
-                "telefone VARCHAR(16),"
-                "url TEXT,"
-                "data TIMESTAMPTZ,"
-                "profissional BOOLEAN,"
-                "PRIMARY KEY(id_anuncio));"
+        self.cur.execute("""
+                CREATE TABLE IF NOT EXISTS mytable (
+                    id_anuncio INTEGER,
+                    municipio VARCHAR(64),
+                    estado CHAR(2),
+                    cep CHAR(8),
+                    preco NUMERIC(14,2),
+                    area VARCHAR(16),
+                    tipo VARCHAR(32),
+                    titulo VARCHAR(64),
+                    descricao TEXT,
+                    fotos TEXT [],
+                    ddd CHAR(2),
+                    telefone VARCHAR(16),
+                    url TEXT,
+                    data TIMESTAMPTZ,
+                    profissional BOOL,
+                    PRIMARY KEY (id_anuncio)
+                ); """
             )
+        self.conn.commit()
 
     def insert_data(self, data):
-        keys = data.keys()
         for values in data.values():
-            self.cur.execute(
-                "INSERT INTO test (%(id_anuncio)s, %(municipio)s, %(estado)s, %(cep)s, %(preco)s, %(area)s, %(tipo)s, %(titulo)s, %(descricao)s, %(fotos)s, %(ddd)s, %(telefone)s, %(url)s, %(data)s, %(profissional)s);"
+            print(values["area"])
+            self.cur.execute("""
+                INSERT INTO mytable (id_anuncio, municipio, estado, cep, preco, area, tipo, titulo, descricao, fotos, ddd, telefone, url, data, profissional)
+                VALUES(%s, %s, %s, %s, %s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s, %s ,%s);""",
+                (values["id_anuncio"],
+                values["municipio"],
+                values["estado"],
+                values["cep"],
+                re.sub(r"(R\$\s|\.)", "", values["preco"]),
+                values["area"],
+                values["tipo"],
+                values["titulo"],
+                values["descricao"],
+                values["fotos"],
+                values["ddd"],
+                values["telefone"],
+                values["url"],
+                values["data"],
+                values["profissional"])
             )
-
 
     def __del__(self):
         self.cur.close()

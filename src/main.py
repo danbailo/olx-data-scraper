6f9f07ea12dcdfaa0b74eff252628780e545d61b
Daniel Bailo <danbailoufms@gmail.com>
2020-02-26 15:28:03 -0400

    tqdm para acompanhemento da coleta e insercao dos dados e configuracao da paralelizacao por processos

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ from core import Olx, Database
 import os
 import json
 import time
+from tqdm import tqdm
 from multiprocessing import Pool
 
 if __name__ == "__main__":
 
-    path = os.path.join(".", "input.txt")
-    db = Database("mydb", "postgres", "59228922ddd")
+    input_file = os.path.join(".", "input.txt")
+    database = Database("mydb", "postgres", "59228922ddd")
 
-    olx = Olx(path)
+    olx = Olx()
 
-    all_urn = olx.get_urn()
-
+    all_urn = olx.get_urn(input_file)
 
     print("\nColetando todas as paginas...")
     pages = olx.get_pages(all_urn)
@@ -20,33 +20,32 @@ if __name__ == "__main__":
 
     print("\nColetando links de todos os anuncios...")
     pool = Pool(8)
-    start_links = time.time() #TIMER
     links = []
     for link in pool.map(olx.get_links, pages):
         links.extend(link)
     pool.close()
     pool.join()        
-    end_links = time.time() #TIMER
     del pool   
+    print("{len_links} Links coletados!".format(len_links=len(links)))
 
-    print("{len_links} Links coletados!\n".format(len_links=len(links)))
-    print("time to get links:", end_links-start_links)
 
     pool = Pool(32)
-
     print("\nColetando dados dos anuncios...")
-    start_collect = time.time() #TIMER
-    data = pool.map(olx.get_json, links)
+    data = pool.map(olx.get_json, tqdm(links))
     pool.close()
     pool.join()
-    end_collect = time.time() #TIMER
-    print("Dados coletados!\n")
-    print("time collect:", end_collect-start_collect)
-
     del pool
+    print("Dados coletados!\n")
 
     print("\nInserindo dados no banco...")
-    db.insert_data(data)
+    database.insert_data(data)
     print("Dados inseridos!\n")
+
+    set_data = set()
+    for row in data:
+        set_data.add(row[0])
+
+    print("Foram inseridos {set_data} tuplas na base dados!".format(set_data=len(set_data)))
+    print("{duplicate} anuncios duplicados no total!\n".format(duplicate=len(data) - len(set_data)))
     
     
